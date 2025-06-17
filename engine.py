44b5984d5a6df4bfaaa4f4fbb95da6a429fba6ee
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-17 15:19:15 +0100

    start building database engine

---
+++ engine.py
@@ -0,0 +1,25 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///steel_production_plan.db")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    """Create the database tables."""
+    Base.metadata.create_all(bind=engine)
+    print("Database initialized.")
+
+
+def get_db():
+    """Get a database session."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
