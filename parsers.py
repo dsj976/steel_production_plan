757b06226a346eb998a6fd3c26b1fd38835a00a6
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-18 15:18:53 +0100

    update parse_daily_schedule function to include 'Heat' column and grade addition logic to grades table

--- parsers.py
+++ parsers.py
@@ -2,14 +2,17 @@ import pandas as pd
 from io import BytesIO
 from sqlalchemy.orm import Session
 
+from models import Grade, DailySchedule
+
 
 def parse_daily_schedule(contents: bytes, db: Session):
     """
     Parse the `daily_charge_schedule.xlsx` file and upload to database.
     """
 
-    # Perform Pandas operations to obtain DataFrame with columns:
-    # ["Date", "Start time", "Grade", "Mould size"]
+    # Perform Pandas pre-processing to obtain DataFrame with columns:
+    # ["Date", "Start time", "Grade", "Mould size", "Heat"], where
+    # "Heat" refers to the heat number of the day.
 
     df = pd.read_excel(BytesIO(contents), header=[1, 2], na_values=["-", "N/A", ""])
     df = df.stack(level=0, future_stack=True)
@@ -19,6 +22,21 @@ def parse_daily_schedule(contents: bytes, db: Session):
     df.sort_values(["Date", "Start time"], inplace=True)
     df.dropna(subset=["Start time", "Grade"], inplace=True)
     df.reset_index(drop=True, inplace=True)
+    df["Heat"] = df.groupby("Date")["Start time"].rank(method="first").astype(int)
+
+    # iterate through rows and add entries to DB
+    grades = []
+    for _, row in df.iterrows():
+        grade_name = row["Grade"].strip()
+        if grade_name not in grades:
+            # if grade not in "grades" table, add it
+            grades.append(grade_name)
+            grade = db.query(Grade).filter_by(name=grade_name).first()
+            if not grade:
+                grade = Grade(name=grade_name)
+                db.add(grade)
+
+    db.commit()
 
     return
 
