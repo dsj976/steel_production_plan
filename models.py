6cfd0203b7cc19dae380198a33ad3e1842f8d540
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-18 23:21:43 +0100

    add MonthlyBreakdown database model

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ class Group(Base):
 
     grades = relationship("Grade", back_populates="group")
     heats = relationship("MonthlyGroupPlan", back_populates="group")
+    tons = relationship("MonthlyBreakdown", back_populates="group")
 
     def __repr__(self):
         return f"Group(name='{self.name}')"
@@ -32,6 +33,7 @@ class Grade(Base):
 
     group = relationship("Group", back_populates="grades")
     heats = relationship("DailySchedule", back_populates="grade")
+    tons = relationship("MonthlyBreakdown", back_populates="grade")
 
     def __repr__(self):
         return f"<Grade(name='{self.name}', group='{self.group}')>"
@@ -68,3 +70,24 @@ class MonthlyGroupPlan(Base):
     heats = Column(Integer, nullable=False)
 
     group = relationship("Group", back_populates="heats")
+
+
+class MonthlyBreakdown(Base):
+    """
+    Table to store the monthly breakdown of
+    steel grade production.
+    """
+
+    __tablename__ = "monthly_breakdown"
+    __table_args__ = (
+        UniqueConstraint("month", "grade_id", name="unique_grade_per_month"),
+    )
+
+    id = Column(Integer, primary_key=True)
+    month = Column(Date, nullable=False)
+    group_id = Column(Integer, ForeignKey("groups.id"), nullable=True)
+    grade_id = Column(Integer, ForeignKey("grades.id"), nullable=False)
+    tons = Column(Integer, nullable=False)
+
+    group = relationship("Group", back_populates="tons")
+    grade = relationship("Grade", back_populates="tons")
