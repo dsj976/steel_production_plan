3e604e917b33d183666fb9962a0ab4ae8d02362a
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-18 11:11:52 +0100

    util function to upload Excel files to the database

---
+++ utils.py
@@ -0,0 +1,12 @@
+import requests
+
+base_url = "http://localhost:8000"
+
+def upload_excel(file_path: str, url: str = f"{base_url}/upload"):
+    """Upload an Excel file (.xlsx) to the database."""
+    with open(file_path, "rb") as f:
+        files = {
+            "file": (file_path, f)
+        }
+        response = requests.post(url, files=files)
+    return response
