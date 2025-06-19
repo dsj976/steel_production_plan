5ef59fe9cff923d980a1c751d3dafc7605d0de12
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-19 18:42:32 +0100

    add get_forecast function to utils module

--- utils.py
+++ utils.py
@@ -9,3 +9,9 @@ def upload_excel(file_path: str, url: str = f"{base_url}/upload"):
         files = {"file": (file_path, f)}
         response = requests.post(url, files=files)
     return response
+
+
+def get_forecast(method: str = "average", url: str = f"{base_url}/forecast"):
+    """Get the steel production forecast for the next month."""
+    response = requests.get(url, params={"method": method})
+    return response
