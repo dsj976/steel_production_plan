3f77d6a4e8d86475d9c643d8ec03c080d94d50bb
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2025-06-19 19:03:15 +0100

    make decay rate an optional parameter instead of hardcoding

--- forecast.py
+++ forecast.py
@@ -6,11 +6,13 @@ from models import MonthlyBreakdown
 def calculate_forecast(
     monthly_breakdown: list[MonthlyBreakdown],
     method: str = "average",
-):
+    decay_rate: float = 0.01,
+) -> int:
     """
     Calculate a steel production forecast for the next month based on
     production history using one of two methods: a standard average
     or a weighted average where more recent observations carry more weight.
+    If using a weighted average, you can specify a decay rate.
     """
     supported_methods = ["average", "weighted_average"]
     if method not in supported_methods:
@@ -25,12 +27,11 @@ def calculate_forecast(
     production_history = pd.DataFrame(rows)
     production_history.set_index("month", inplace=True)
     production_history.index = pd.to_datetime(production_history.index)
-    # sort by month
     production_history.sort_index(inplace=True)
     if method == "average":
         forecast = np.average(production_history).round()
     elif method == "weighted_average":
-        decay_rate = 0.01
+        decay_rate = decay_rate
         time_deltas = (production_history.index[-1] - production_history.index).days
         weights = np.exp(-decay_rate * time_deltas)
         forecast = np.average(production_history, weights=weights, axis=0).round()
